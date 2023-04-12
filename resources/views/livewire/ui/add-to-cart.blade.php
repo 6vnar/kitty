5d066d1400d095ba44242d67109a6c73f79e6d1c
Manar Naseer <manarnaseerabdulla@gmail.com>
2023-04-12 15:14:45 +0300

    Update add-to-cart.blade.php

--- resources/views/livewire/ui/add-to-cart.blade.php
+++ resources/views/livewire/ui/add-to-cart.blade.php
@@ -1,8 +1,8 @@
 <div>
+    @section('title', 'Shopping Cart')
 <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
     <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
         <div class="p-6 bg-white border-b border-gray-200">
-            <h1 class="text-2xl font-bold mb-4">Shopping Cart</h1>
             <table class="w-full">
                 <thead>
                     <tr>
