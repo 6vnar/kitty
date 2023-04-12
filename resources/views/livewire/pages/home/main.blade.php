8eee85b68224f78bac5439579f8c0ef4a0d02a6e
Manar Naseer <manarnaseerabdulla@gmail.com>
2023-04-12 13:58:07 +0300

    Update main.blade.php

--- resources/views/livewire/pages/home/main.blade.php
+++ resources/views/livewire/pages/home/main.blade.php
@@ -5,39 +5,20 @@
         gggggggg
     @endauth
     {{-- Recently Added --}}
-    <div class="container mx-auto justfiy-start">
-        <h2 class="text-lg font-medium mb-4">Recently Added</h2>
-        <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
-        @foreach ($recentlyAdded as $product)
-                @livewire('ui.card', ['product' => $product], key($product->id))
-                @endforeach
-    </div>
-    {{-- Most Sold --}}
-        <h2 class="text-lg font-medium mb-4">Most Sold</h2>
-    <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
-                @foreach ($mostSold as $product)
-                @livewire('ui.card', ['product' => $product], key($product->id))
-                @endforeach
-    </div>
-    </div>
+    <div class="container mx-auto justfiy-start mt-6">
+        <h2 class="text-left text-gray-500 text-lg font-bold">Recently Added</h2>
+        <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 container mx-auto shadow-sm ">
 
-    {{-- <div class="container mx-auto">
-        <div class="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3"> --}}
-            {{-- Recently Added --}}
-            {{-- <div>
-                <h2 class="text-lg font-medium mb-4">Recently Added</h2>
-                @foreach ($recentlyAdded as $product)
+            @foreach ($recentlyAdded as $product)
                 @livewire('ui.card', ['product' => $product], key($product->id))
-                @endforeach
-            </div> --}}
-    
-            {{-- Most Sold --}}
-            {{-- <div>
-                <h2 class="text-lg font-medium mb-4">Most Sold</h2>
-                @foreach ($mostSold as $product)
-                @livewire('ui.card', ['product' => $product], key($product->id))
-                @endforeach
-            </div>
+            @endforeach
         </div>
-    </div> --}}
+        {{-- Most Sold --}}
+        <h2 class="text-left text-gray-500 text-lg font-bold mt-6">Most Sold</h2>
+        <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 container mx-auto shadow-sm">
+            @foreach ($mostSold as $product)
+                @livewire('ui.card', ['product' => $product], key($product->id))
+            @endforeach
+        </div>
+    </div>
 </div>
