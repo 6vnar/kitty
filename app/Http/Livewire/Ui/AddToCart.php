5018a3523e2abcab091c5f479ea10f258758da14
Manar Naseer <manarnaseerabdulla@gmail.com>
2023-05-05 12:57:02 +0300

    ad

--- app/Http/Livewire/Ui/AddToCart.php
+++ app/Http/Livewire/Ui/AddToCart.php
@@ -11,8 +11,8 @@ use Jantinnerezo\LivewireAlert\LivewireAlert;
 class AddToCart extends Component
 {
     use LivewireAlert;
-    protected $listeners = ['cartUpdated','removeFromCart','confirmDelete','refresh'];
-    public $cart, $cart_id;
+    protected $listeners = ['cartUpdated', 'removeFromCart', 'confirmDelete', 'refresh'];
+    public $cart, $cart_id, $quantity = 1;
 
     //remove from cart function
     public function removeFromCart()
@@ -20,14 +20,13 @@ class AddToCart extends Component
         $cart = Cart::where('id', $this->cart_id->id)
             ->where('user_id', Auth::user()->id)
             ->first();
-        
+
         if ($cart) {
             // remove cart from carts table
             $cart->delete();
-            
+
             // remove product from cart_product table
             $cart->products()->detach($this->cart_id->id);
-            
             $this->alert('success', 'Product removed from cart successfully!', [
                 'position' => 'top',
                 'timer' => 3000,
@@ -35,10 +34,10 @@ class AddToCart extends Component
             ]);
             $this->emitUp('$refresh');
         }
-        
+
         return redirect()->to('/add-to-cart');
     }
-    
+
 
     public function confirmDelete($id)
     {
@@ -53,11 +52,57 @@ class AddToCart extends Component
         ]);
     }
 
+    public function increment($id)
+    {
+        //update quantity column in carts table
+        $cart = Cart::findOrfail($id)
+            ->where('user_id', Auth::user()->id)
+            ->first();
     
+        if ($cart) {
+            $cart->increment('quantity');
+            $this->alert('success', 'Product quantity updated successfully!', [
+                'position' => 'top',
+                'timer' => 3000,
+                'toast' => true,
+            ]);
+            //update total_price column in carts table
+            $cart->update([
+                'total_price' => $cart->price * $cart->quantity
+            ]);
+            $this->emitUp('$refresh');
+        }   
+    }
+    
+    public function decrement($id)
+    {
+        //update quantity column in carts table
+        $cart = Cart::findOrFail($id);
+    
+        if ($cart && $cart->user_id == Auth::user()->id) {
+            $cart->decrement('quantity');
+            $this->alert('success', 'Product quantity updated successfully!', [
+                'position' => 'top',
+                'timer' => 3000,
+                'toast' => true,
+            ]);
+            //update total_price column in carts table
+            $cart->update([
+                'total_price' => $cart->price * $cart->quantity
+            ]);
+            $this->emitUp('$refresh');
+        }
+    }
+    
+
+
     public function render()
     {
         $carts = Cart::where('user_id', Auth::user()->id)->with('products')->get();
-        
+        if ($this->cart !== null) {
+            $cart['total_price'] = $this->cart['price'] * $this->cart['quantity'];
+        }
+
         return view('livewire.ui.add-to-cart', compact('carts'));
     }
 }
--- resources/views/livewire/ui/add-to-cart.blade.php
+++ resources/views/livewire/ui/add-to-cart.blade.php
@@ -1,58 +1,69 @@
 <div>
-    @section('title',__('ui.Shopping Cart'))
+    @section('title', __('ui.Shopping Cart'))
     <div class="max-w-8xl mx-auto py-6 sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
             <div class="p-6 bg-white border-b border-gray-200">
                 @if (count($carts) > 0)
-                @foreach($carts as $cart)
-                {{-- <h2 class="text-lg font-bold">{{ $cart->id }}</h2> --}}
-                <table class="w-full">
-                    <thead>
-                        <tr>
-                            <th class=" @if (app()->getLocale() == 'ar') text-right @else text-left  @endif"> {{ __('ui.Product') }}</th>
-                            <th class=" @if (app()->getLocale() == 'ar') text-right @else text-left  @endif"> {{ __('ui.Price') }}</th>
-                            <th class="@if (app()->getLocale() == 'ar') text-right @else text-left  @endif"> {{ __('ui.Quantity') }}</th>
-                            <th class="@if (app()->getLocale() == 'ar') text-left @else text-right   @endif"> {{ __('ui.Subtotal') }}</th>
-                            
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($cart->products as $product)
-                        <tr>
-                            <td>
-                                <div class="flex items-center">
-                                    <img class="w-12 h-12 mr-4 @if (app()->getLocale() == 'ar') ml-2 @else  @endif" src="{{ asset('images/clothes/ok.png') }}" alt="Product">
-                                    <span class="font-medium w-12 h-12 mr-4 text-black"> {{ $product['name'] }}</span>
-                                </div>
-                            </td>
-                            <td>{{ $product['price'] }}</td>
-                            <td>
-                                <div class="flex items-center">
-                                    <button class="p-1 border rounded-lg" onclick="decrementQuantity()">-</button>
-                                    <span id="quantity" class="px-2">{{ $cart['quantity'] }}</span>
-                                    <button class="p-1 border rounded-lg" onclick="incrementQuantity()">+</button>
-                                </div>
-                            </td>
-                            <td class="@if (app()->getLocale() == 'ar') text-left @else text-right   @endif" id="subtotal" class="ml-4">${{ $cart['price'] * $cart['quantity'] }}</td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-                <div class="mt-6 @if (app()->getLocale() == 'ar') text-left @else text-right   @endif">
-                    <button class="px-4 py-2 bg-pink-500 text-white rounded-lg"> {{ __('ui.Checkout') }}</button>
-                    <button wire:click="confirmDelete({{ $cart->id }})" class="px-4 py-2 bg-pink-500 text-white rounded-lg">{{ __('ui.delete') }}</button>
-                </div>
-                @endforeach
+                    @foreach ($carts as $cart)
+                        {{-- <h2 class="text-lg font-bold">{{ $cart->id }}</h2> --}}
+                        <table class="w-full">
+                            <thead>
+                                <tr>
+                                    <th class=" @if (app()->getLocale() == 'ar') text-right @else text-left @endif">
+                                        {{ __('ui.Product') }}</th>
+                                    <th class=" @if (app()->getLocale() == 'ar') text-right @else text-left @endif">
+                                        {{ __('ui.Price') }}</th>
+                                    <th class="@if (app()->getLocale() == 'ar') text-right @else text-left @endif">
+                                        {{ __('ui.Quantity') }}</th>
+                                    <th class="@if (app()->getLocale() == 'ar') text-left @else text-right @endif">
+                                        {{ __('ui.Subtotal') }}</th>
+
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($cart->products as $product)
+                                    <tr>
+                                        <td>
+                                            <div class="flex items-center">
+                                                <img class="w-12 h-12 mr-4 @if (app()->getLocale() == 'ar') ml-2 @else @endif"
+                                                    src="{{ asset('images/clothes/ok.png') }}" alt="Product">
+                                                <span class="font-medium w-12 h-12 mr-4 text-black">
+                                                    {{ $product['name'] }}</span>
+                                            </div>
+                                        </td>
+                                        <td>{{ $product['price'] }}</td>
+                                        <td>
+                                            <div class="flex items-center">
+                                                <button class="p-1 border rounded-lg"
+                                                wire:click="decrement({{ $cart->id }})">-</button>
+                                                <span id="quantity" class="px-2">{{ $cart['quantity'] }}</span>
+                                                <button class="p-1 border rounded-lg"
+                                                wire:click="increment({{ $cart->id }})">+</button>
+                                            </div>
+                                        </td>
+                                        <td class="@if (app()->getLocale() == 'ar') text-left @else text-right @endif"
+                                            class="ml-4">${{ $cart['total_price']}}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                        <div class="mt-6 @if (app()->getLocale() == 'ar') text-left @else text-right @endif">
+                            <button class="px-4 py-2 bg-pink-500 text-white rounded-lg">
+                                {{ __('ui.Checkout') }}</button>
+                            <button wire:click="confirmDelete({{ $cart->id }})"
+                                class="px-4 py-2 bg-pink-500 text-white rounded-lg">{{ __('ui.delete') }}</button>
+                        </div>
+                    @endforeach
                 @else
-                <p class="text-xl">{{ __('ui.Your cart is empty.') }}</p>
+                    <p class="text-xl">{{ __('ui.Your cart is empty.') }}</p>
                 @endif
             </div>
         </div>
     </div>
 </div>
-<script>
+{{-- <script>
     let price = 0;
-    @if(isset($cart['price']))
+    @if (isset($cart['price']))
         price = {{ $cart['price'] }};
     @endif
 
@@ -78,4 +89,4 @@
         const subtotalElement = document.getElementById('subtotal');
         subtotalElement.innerText = `$${price * quantity}`;
     }
-</script>
+</script> --}}
